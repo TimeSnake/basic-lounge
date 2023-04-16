837bf5608a7dde0980b94fa56272ffaaf8f28e86
funzter <funzter@mail.timesnake.de>
2023-04-16 13:49:14 +0200

    refactored kit selection

--- src/main/java/de/timesnake/basic/lounge/kit/KitSelection.java
+++ src/main/java/de/timesnake/basic/lounge/kit/KitSelection.java
@@ -6,6 +6,7 @@ package de.timesnake.basic.lounge.kit;
 
 import de.timesnake.basic.bukkit.util.user.inventory.ExInventory;
 import de.timesnake.basic.bukkit.util.user.inventory.ExItemStack;
+import de.timesnake.basic.bukkit.util.user.inventory.UserInventoryClickListener;
 import de.timesnake.basic.game.util.game.Kit;
 import de.timesnake.basic.game.util.server.GameServer;
 import de.timesnake.basic.lounge.chat.Plugin;
@@ -13,11 +14,9 @@ import de.timesnake.basic.lounge.server.LoungeServer;
 import de.timesnake.basic.lounge.user.LoungeUser;
 import de.timesnake.library.basic.util.Loggers;
 import de.timesnake.library.chat.ExTextColor;
-import java.util.ArrayList;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemFlag;
 
 public class KitSelection {
 
@@ -50,7 +49,7 @@ public class KitSelection {
                 (int) (9 * Math.ceil(GameServer.getGame().getKits().size() / 7.0)),
                 Component.text("Kitselection"));
 
-        ExItemStack item = this.createKitItem(Kit.RANDOM);
+        ExItemStack item = Kit.RANDOM.createDisplayItem(this.getKitClickListener(Kit.RANDOM));
         this.inventory.setItemStack(0, item);
 
         int i = 2;
@@ -58,36 +57,31 @@ public class KitSelection {
             while (i % 9 == 0) {
                 i += 2;
             }
-            item = this.createKitItem(kit);
+            item = kit.createDisplayItem(this.getKitClickListener(kit));
             this.inventory.setItemStack(i, item);
             i++;
         }
     }
 
-    private ExItemStack createKitItem(Kit kit) {
-        return new ExItemStack(kit.getMaterial())
-                .setDisplayName(kit.getName())
-                .setExLore(new ArrayList<>(kit.getDescription()))
-                .addExItemFlags(ItemFlag.HIDE_DYE, ItemFlag.HIDE_PLACED_ON,
-                        ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_DESTROYS)
-                .onClick(event -> {
-                    LoungeUser user = ((LoungeUser) event.getUser());
+    private UserInventoryClickListener getKitClickListener(Kit kit) {
+        return event -> {
+            LoungeUser user = ((LoungeUser) event.getUser());
 
-                    if (LoungeServer.getGameCountdown() <= LoungeServer.KIT_SELECTION_CLOSED) {
-                        user.sendPluginMessage(Plugin.LOUNGE,
-                                Component.text("The kit selection is closed", ExTextColor.WARNING));
-                        return;
-                    }
+            if (LoungeServer.getGameCountdown() <= LoungeServer.KIT_SELECTION_CLOSED) {
+                user.sendPluginMessage(de.timesnake.basic.game.util.user.Plugin.LOUNGE,
+                        Component.text("The kit selection is closed", ExTextColor.WARNING));
+                return;
+            }
 
-                    user.setSelectedKit(kit);
-                    user.sendPluginMessage(Plugin.LOUNGE,
-                            Component.text("You selected kit ", ExTextColor.PERSONAL)
-                                    .append(Component.text(kit.getName(), ExTextColor.VALUE)));
-                    Loggers.LOUNGE.info(user.getName() + " selected kit " + kit.getName());
+            user.setSelectedKit(Kit.RANDOM);
+            user.sendPluginMessage(de.timesnake.basic.game.util.user.Plugin.LOUNGE,
+                    Component.text("You selected kit ", ExTextColor.PERSONAL)
+                            .append(Component.text(Kit.RANDOM.getName(), ExTextColor.VALUE)));
+            Loggers.LOUNGE.info(user.getName() + " selected kit " + Kit.RANDOM.getName());
 
-                    user.closeInventory();
-                    event.setCancelled(true);
-                });
+            user.closeInventory();
+            event.setCancelled(true);
+        };
     }
 
     public Inventory getInventory() {
