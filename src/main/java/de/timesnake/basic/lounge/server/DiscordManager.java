5c976dae7fcb486221954e83c69bf231c27e83cd
funzter <funzter@mail.timesnake.de>
2023-12-30 18:21:26 +0100

    dependency update

--- src/main/java/de/timesnake/basic/lounge/server/DiscordManager.java
+++ src/main/java/de/timesnake/basic/lounge/server/DiscordManager.java
@@ -9,7 +9,7 @@ import de.timesnake.basic.bukkit.util.user.User;
 import de.timesnake.basic.lounge.user.LoungeUser;
 import de.timesnake.channel.util.message.ChannelDiscordMessage;
 import de.timesnake.channel.util.message.MessageType;
-import de.timesnake.database.util.object.Type;
+import de.timesnake.library.basic.util.DiscordChannelType;
 
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -23,38 +23,32 @@ public class DiscordManager {
       return;
     }
 
-    if (LoungeServer.getGame().getDiscordType().equals(Type.Discord.TEAMS)) {
+    if (LoungeServer.getGame().getDiscordType().equals(DiscordChannelType.TEAMS)) {
       LinkedHashMap<String, List<UUID>> uuidsByTeam = new LinkedHashMap<>();
       for (User user : LoungeServer.getUsers()) {
-        List<UUID> uuids = uuidsByTeam.computeIfAbsent(
-            ((LoungeUser) user).getTeam().getDisplayName(),
-            k -> new LinkedList<>());
+        List<UUID> uuids = uuidsByTeam.computeIfAbsent(((LoungeUser) user).getTeam().getDisplayName(), k -> new LinkedList<>());
         uuids.add(user.getUniqueId());
       }
 
       uuidsByTeam.put(LoungeServer.DISCORD_SPECTATOR, List.of());
       uuidsByTeam.put(LoungeServer.DISCORD_LOUNGE, List.of());
 
-      Server.getChannel()
-          .sendMessage(new ChannelDiscordMessage<>(LoungeServer.getGameServer().getName(),
-              MessageType.Discord.MOVE_MEMBERS, new ChannelDiscordMessage.Allocation(uuidsByTeam)));
-    } else if (LoungeServer.getGame().getDiscordType().equals(Type.Discord.DISTANCE)) {
+      Server.getChannel().sendMessage(new ChannelDiscordMessage<>(LoungeServer.getGameServer().getName(),
+          MessageType.Discord.MOVE_MEMBERS, new ChannelDiscordMessage.Allocation(uuidsByTeam)));
+    } else if (LoungeServer.getGame().getDiscordType().equals(DiscordChannelType.DISTANCE)) {
       LinkedHashMap<String, List<UUID>> uuidsByTeam = new LinkedHashMap<>();
 
       uuidsByTeam.put(LoungeServer.DISCORD_SPECTATOR, List.of());
-      uuidsByTeam.put(LoungeServer.DISCORD_LOUNGE,
-          LoungeServer.getUsers().stream().map(User::getUniqueId).toList());
+      uuidsByTeam.put(LoungeServer.DISCORD_LOUNGE, LoungeServer.getUsers().stream().map(User::getUniqueId).toList());
 
-      Server.getChannel()
-          .sendMessage(new ChannelDiscordMessage<>(LoungeServer.getGameServer().getName(),
-              MessageType.Discord.MOVE_MEMBERS, new ChannelDiscordMessage.Allocation(uuidsByTeam)));
+      Server.getChannel().sendMessage(new ChannelDiscordMessage<>(LoungeServer.getGameServer().getName(),
+          MessageType.Discord.MOVE_MEMBERS, new ChannelDiscordMessage.Allocation(uuidsByTeam)));
     }
 
   }
 
   public void cleanup() {
-    Server.getChannel()
-        .sendMessage(new ChannelDiscordMessage<>(LoungeServer.getGameServer().getName(),
-            MessageType.Discord.DESTROY_CHANNELS, new LinkedList<>()));
+    Server.getChannel().sendMessage(new ChannelDiscordMessage<>(LoungeServer.getGameServer().getName(),
+        MessageType.Discord.DESTROY_CHANNELS, new LinkedList<>()));
   }
 }
